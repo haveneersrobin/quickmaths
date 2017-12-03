f93eccea94680236eb6bfd0f7dc1b15853a481a3
Laurens Cleemput <laurenscleemput@hotmail.com>
2017-12-03 13:06:59 +0100

    Added multiplications

--- util/GridGenerator.js
+++ util/GridGenerator.js
@@ -522,6 +522,196 @@ export function createModuloGrid(divider, maxNumber, levelLength, nbCols = 3) {
 }
 
 /**
+ * Returns all dividers of a number, needed for multiplication exercises
+ * @param {*} num The number for which dividers are needed
+ */
+export function getDividers(num) {
+    var half = Math.floor(num / 2), // Ensures a whole number <= num.
+        divs = [1],
+        i, j;
+
+    // Determine out increment value for the loop and starting point.
+    num % 2 === 0 ? (i = 2, j = 1) : (i = 3, j = 2);
+
+    for (i; i <= half; i += j) {
+        num % i === 0 ? divs.push(i) : false;
+    }
+    divs.push(num); // num will be a part of every solution.
+
+    return divs;
+}
+
+/**
+ * Creates a row with multiplication questions
+ * @param {*} solution - The desired solution
+ * @param {*} lowerbound - The lower bound of results to be generated
+ * @param {*} upperbound - The upper bound of results to be generated
+ * @param {*} nbCols - The amount of columns on the grid, default: 3
+ */
+export function getMultiplicationRow(solution, lowerbound, upperbound, nbCols = 3) {
+
+    if (solution === undefined || solution === null || solution === -1) {
+        console.log("No correct solution supplied to getMultiplicationRow ! Solution is " + typeof solution);
+        return;
+    }
+
+    if (lowerbound > upperbound || lowerbound > solution) {
+        console.log('solution should be a number between lowerbound and upperbound!');
+        return;
+    }
+
+    // Init constants
+    const fillRate = 0.85; // The ratio of filled tiles to total tiles
+    const correctRate = 0.45; // The ratio of correct tiles to filled tiles
+
+    var hasCorrectTile = false; // Denotes whether a row already has a correct tile
+    // currentRow will be added to the grid elem of result
+    var currentRow = [];
+    // currentNum is the randomly generated integer
+    var currentNum = -1; // Default number
+
+    // Generate only one row
+    var i = 0;
+    while (i < 1) {
+        // Reset hasCorrectTile flag
+        hasCorrectTile = false;
+        // Clear temp var currentRow
+        currentRow = [];
+        for (var j = 0; j < nbCols; j++) {
+
+            // Init columnElem
+            // string = string representation on grid
+            // numResult = result of given string (int)
+            var currentElem = {
+                string: '',
+                numResult: -1,
+                correct: false
+            }
+
+            // Generate random number
+            if (Math.random() <= fillRate) {
+                // Generate random integer
+                // Reset temp var currentNum
+                currentNum = solution; // Init with illegal number
+                // Make sure that the random number is not equal to the solution, this eliminates the edge case
+                // Also verify that the random number has at least 4 dividers
+                while (currentNum === solution) {
+                    currentNum = _.random(lowerbound, upperbound);
+                    while (getDividers(currentNum).length <= 4) {
+                        // CurrentNum needs to have at least 4 divs
+                        currentNum = _.random(lowerbound, upperbound);
+                    }
+                }
+            }
+            else {
+                // Else fill in -1.
+                currentNum = -1;
+            }
+
+            // Logic about tile filling
+            if (!hasCorrectTile && currentNum != -1) {
+                // Test whether to fill with correct number or not
+                if (Math.random() <= correctRate) {
+                    // Add correct tile
+                    // Set flag and generate parts of sum
+                    hasCorrectTile = true;
+                    var divs = getDividers(solution);
+                    var nbDivs = divs.length - 1; // -1 for indexing purposes
+                    var index = _.random(1, nbDivs-1); // Prevent 1 . x or x . 1
+                    var firstDiv = divs[index];
+                    var secondDiv = divs[nbDivs - index];
+
+                    currentElem.string = firstDiv + '.' + secondDiv;
+                    currentElem.numResult = solution;
+                    currentElem.correct = true;
+                }
+                else {
+                    // Else generate parts of sum of wrong tile
+                    var divs = getDividers(currentNum);
+                    var nbDivs = divs.length - 1; // -1 for indexing purposes
+                    var index = _.random(1, nbDivs-1); // Prevent 1 . x or x . 1
+                    var firstDiv = divs[index];
+                    var secondDiv = divs[nbDivs - index];
+
+                    currentElem.string = firstDiv + '.' + secondDiv;
+                    currentElem.numResult = currentNum
+                }
+            }
+
+            else if (currentNum != -1) {
+                // Else generate parts of sum of wrong tile
+                var divs = getDividers(currentNum);
+                var nbDivs = divs.length - 1; // -1 for indexing purposes
+                var index = _.random(1, nbDivs-1); // Prevent 1 . x or x . 1
+                var firstDiv = divs[index];
+                var secondDiv = divs[nbDivs - index];
+
+                currentElem.string = firstDiv + '.' + secondDiv;
+                currentElem.numResult = currentNum
+            }
+            // Else leave elem as default, no action needed
+
+            // Push new elemenent into row
+            currentRow.push(currentElem);
+        }
+
+        // Check if row is empty
+        var filled = false;
+        for (var k = 0; k < nbCols; k++) {
+            if (currentRow[k].numResult !== -1) {
+                filled = true;
+            }
+        }
+
+        // if row is empty, try again (don't increment i)
+        // if row is not empty, increment i => end loop
+        if (filled) {
+            i++; // Increment row counter, row is OK
+        }
+    }
+    return currentRow;
+}
+
+/**
+ * Creates a grid (levelLength x nbCols) with multiplication questions
+ * @param {*} solution - The result of the sum
+ * @param {*} lowerbound - The lower bound of results to be generated
+ * @param {*} upperbound - The heighest possible number to be generated
+ * @param {*} levelLength - The height of the grid
+ * @param {*} nbCols - The width of the grid, default: 3
+ */
+export function createMultiplicationGrid(solution, lowerbound, upperbound, levelLength, nbCols = 3) {
+
+    // Init result
+    var result = {
+        objective: 'Welke vermenigvuldiging is gelijk aan ' + solution + '?',
+        numericSolution: solution,
+        grid: []
+    }
+
+    // Fill levelLength amount of rows
+    for (var i = 0; i < levelLength; i++) {
+        result.grid.push(getMultiplicationRow(solution, lowerbound, upperbound, nbCols));
+    }
+
+    result.grid.push([{
+        "string": "",
+        "numResult": -1,
+        "correct": true
+    },{
+        "string": "",
+        "numResult": -1,
+        "correct": true
+    },{
+        "string": "",
+        "numResult": -1,
+        "correct": true
+    }]);
+
+    return result;
+}
+
+/**
  * Returns a grid of a random type, given a specified difficulty for the Classic Mode
  * @param {*} level - The number of the current level, specifying the difficulty (length and range of numbers)
  * @param {*} nbCols - The width of the grid, default: 3
@@ -531,7 +721,7 @@ export function getRandomGridByDiffClassic(level, nbCols = 3) {
     var levelLength = Math.floor(5.69 * Math.log(level) + 3.04);
 
     // First, generate the kind of level
-    const levelKinds = 4;
+    const levelKinds = 5;
     switch (_.random(1, levelKinds)) {
 
         // Generate Sum Grid
@@ -576,6 +766,20 @@ export function getRandomGridByDiffClassic(level, nbCols = 3) {
             // numbers will be generated between solution - 1/2 * range and solution + 1/2 * range
             return createMixedGrid(solution, Math.round(solution - (range / 2)), Math.round(solution + (range / 2)), levelLength, nbCols);
 
+        // Generate Multiplication Grid
+        case 5:
+            // Set solution
+            var solution = _.random(level * 5 +10, level * 7+10);
+            while (getDividers(solution).length <= 4) {
+                // CurrentNum needs to have at least 4 divs
+                solution = _.random(level * 5 +10, level * 7+10);
+            }
+
+            // range = range of possible answers to be generated
+            var range = _.random(level * 3+5, level * 4+5);
+            // numbers will be generated between solution - 1/2 * range and solution + 1/2 * range
+            return createMultiplicationGrid(solution, Math.round(solution - (range / 2)), Math.round(solution + (range / 2)), levelLength, nbCols);
+
         default:
             console.log('Oops, this kind of level does not exist, yet');
     }
@@ -583,7 +787,6 @@ export function getRandomGridByDiffClassic(level, nbCols = 3) {
 
 /**
  * Returns a grid of a random type, given a specified difficulty for the Endurance Mode
- * This mode generates slightly easier levels
  * @param {*} level - The number of the current level, specifying the difficulty (length and range of numbers)
  * @param {*} nbCols - The width of the grid, default: 3
  */
@@ -592,7 +795,7 @@ export function getRandomGridByDiffEndurance(level, nbCols = 3) {
     var levelLength = Math.floor(0.75 * level * Math.log(level) + 3.04);
 
     // First, generate the kind of level
-    const levelKinds = 4;
+    const levelKinds = 5;
     switch (_.random(1, levelKinds)) {
 
         // Generate Sum Grid
@@ -636,6 +839,20 @@ export function getRandomGridByDiffEndurance(level, nbCols = 3) {
             var range = _.random(level * 2, level * 3);
             // numbers will be generated between solution - 1/2 * range and solution + 1/2 * range
             return createMixedGrid(solution, Math.round(solution - (range / 2)), Math.round(solution + (range / 2)), levelLength, nbCols);
+
+        // Generate Multiplication Grid
+        case 5:
+            // Set solution
+            var solution = _.random(level * 3 +20, level * 4+20);
+            while (getDividers(solution).length <= 4) {
+                // CurrentNum needs to have at least 4 divs
+                solution = _.random(level * 3 +20, level * 4+20);
+            }
+
+            // range = range of possible answers to be generated
+            var range = _.random(level * 2+5, level * 3+5);
+            // numbers will be generated between solution - 1/2 * range and solution + 1/2 * range
+            return createMultiplicationGrid(solution, Math.round(solution - (range / 2)), Math.round(solution + (range / 2)), levelLength, nbCols);
 
         default:
             console.log('Oops, this kind of level does not exist, yet');
